7e3825e6be0a43ce81a361bab19bce10eede2b72
Mikha Johnson <nnatasha1027@gmail.com>
2021-06-11 16:06:49 +0530

    changes to test

--- FoodApp/app/src/main/java/com/mj/foodapp/util/Constants.kt
+++ FoodApp/app/src/main/java/com/mj/foodapp/util/Constants.kt
@@ -3,7 +3,7 @@ package com.mj.foodapp.util
 class Constants {
     companion object{
         const val BASE_URL = "https://api.spoonacular.com"
-        const val API_KEY = "[API KEY]"
+        const val API_KEY = "[API KEY_ADD_HERE]"
 
         // API Query Keys
         const val QUERY_NUMBER = "number"
